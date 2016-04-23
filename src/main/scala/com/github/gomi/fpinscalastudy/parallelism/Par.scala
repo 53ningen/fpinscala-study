8f1f853f466dcef23e7fb0d8797cbbcac7bce9e6
gomi_ningen <korun24276@gmail.com>
2016-04-23 21:42:41 +0900

    ParのI/F定義（〜P129）

---
+++ src/main/scala/com/github/gomi/fpinscalastudy/parallelism/Par.scala
@@ -0,0 +1,50 @@
+package com.github.gomi.fpinscalastudy.parallelism
+
+import java.util.concurrent.{Callable, TimeUnit, Future, ExecutorService}
+
+trait Par[+A]
+
+object Par {
+
+  type Par[A] = ExecutorService => Future[A]
+
+  // 直ちに a 値が得られる計算を作成
+  def unit[A](a: A): Par[A] = ???
+
+  // 式 a を run による並列評価のためにマッピング
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  // 与えられた Par を完全に評価し、fork によって要求される並列計算を生成し、結果の値を取得
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  // run による並列評価の対象としてマーク
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
+    override def call(): A = a(es).get
+  })
+
+  //  def sum(ints: IndexedSeq[Int]): Par[Int] =
+  //    if (ints.size <= 1) {
+  //      Par.unit(ints.headOption.getOrElse(0))
+  //    } else {
+  //      val (l, r) = ints.splitAt(ints.length/2)
+  //      Par.map2(sum(l), sum(r))(_ + _)
+  //    }
+
+  // 2つの並列計算の結果を2項関数で結合
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone: Boolean = true
+
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+}
